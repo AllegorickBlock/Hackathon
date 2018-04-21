052d21c7bf38c7cc046de2bd2e7596ed36a4d416
Ragnarokeur <38569852+Ragnarokeur@users.noreply.github.com>
2018-04-21 14:36:09 +0200

    etouemagueule

---
+++ Hackaton/Assets/script/textToSPEECH.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SpeechLib;
+using UnityEngine.UI;
+
+using UnityEngine.Windows.Speech;
+
+public class textToSPEECH : MonoBehaviour
+{
+    public UnityEngine.UI.Text texto;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ButtonPress();
+        }
+
+    }
+
+    public void ButtonPress()
+    {
+        SpVoice voice;
+        voice = new SpVoice();
+        voice.Speak("salut");
+        voice.Speak(texto.text);
+
+    }
+
+
+}
+
